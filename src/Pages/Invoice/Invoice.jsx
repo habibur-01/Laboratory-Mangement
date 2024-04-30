d027bcce919d421b86129b690115476f37863cfc
habibur-01 <h.r.habibur08@gmail.com>
2024-05-01 05:00:37 +0600

    added invoice component, pagiantion & search functionality

--- src/Pages/Invoice/Invoice.jsx
+++ src/Pages/Invoice/Invoice.jsx
@@ -7,11 +7,10 @@ import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 const Invoice = () => {
     const [invoices, setInvoices] = useState([])
-    const [itemsPerpage, setItemsPerPage] = useState(5)
-    const [currentPage, setCurrentPage] = useState(0)
+    const [itemsPerPage, setItemsPerPage] = useState(5)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [currentInvoices, setCurrentInvoices] = useState([])
 
-    const numOfPages = Math.ceil((invoices.length) / itemsPerpage)
-    const pages = [...Array(numOfPages).keys()]
     useEffect(() => {
         axios.get("/invoice.json")
             .then(response => {
@@ -21,94 +20,115 @@ const Invoice = () => {
                 console.log(err)
             })
     }, [])
-    console.log(typeof itemsPerpage)
+
     const handlePerPage = (e) => {
         setItemsPerPage(parseInt(e.target.value))
-        setCurrentPage(0)
+        setCurrentPage(1) // Reset current page to 1 when changing items per page
     }
+
     const handlePrevPage = () => {
-        if (currentPage > 0) {
+        if (currentPage > 1) {
             setCurrentPage(currentPage - 1)
         }
     }
+
     const handleNextPage = () => {
-        if (currentPage < pages.length - 1) {
+        if (currentPage < Math.ceil(invoices.length / itemsPerPage)) {
             setCurrentPage(currentPage + 1)
         }
     }
-    const indexOfLastInvoice = currentPage * itemsPerpage;
-    const indexOfFirstInvoice = indexOfLastInvoice - itemsPerpage;
-    const currentInvoices = invoices.slice(indexOfFirstInvoice, indexOfLastInvoice);
+
+    useEffect(() => {
+        const indexOfLastInvoice = currentPage * itemsPerPage;
+        const indexOfFirstInvoice = indexOfLastInvoice - itemsPerPage;
+        const invoicesData = invoices.slice(indexOfFirstInvoice, indexOfLastInvoice);
+        setCurrentInvoices(invoicesData)
+    }, [invoices, itemsPerPage, currentPage])
+
+    const handleSearch = (e) => {
+        
+        setCurrentPage(1); 
+
+        // Filter invoices based on search term
+        const filteredInvoices = invoices.filter(invoice => invoice.client_code.includes(e.target.value));
+        setCurrentInvoices(filteredInvoices);
+    };
 
     return (
         <div className="px-10">
             <HeaderTitle title={'Invoices'}></HeaderTitle>
             <div>
                 <div>
-                    <label htmlFor="search">Search:</label>
-                    <input type="text" name="code" placeholder="type client code" className="input input-bordered ml-2" required />
-                </div>
-                <div>
-
-                </div>
-                <div>
-                    <div>
+                    <form >
                         <div>
-                            <label htmlFor="">Display</label>
-                            <select value={itemsPerpage} className="ml-2 px-4 py-2 rounded-md border-2" onChange={handlePerPage}>
+                            <label htmlFor="search">Search:</label>
+                            <input type="text" name="code" placeholder="type client code" className="input input-bordered ml-2" required onChange={handleSearch}/>
+                        </div>
+                    </form>
+                </div>
+                <div className="py-10">
+                    <div className="overflow-x-auto">
+                        <table className="table table-zebra">
+                            <thead>
+                                <tr>
+                                    <th>Invoice Number</th>
+                                    <th>Client Code</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Total</th>
+                                    <th>Paid</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    currentInvoices.map(invoice => (
+                                        <tr key={invoice.client_code} className="py-4">
+                                            <td>{invoice.invoice_number}</td>
+                                            <td>{invoice.client_code}</td>
+                                            <td>{invoice.customer.name}</td>
+                                            <td>{invoice.customer.email}</td>
+                                            <td>{invoice.total}</td>
+                                            <td>{invoice.paid}</td>
+                                            <td>{invoice.status}</td>
+                                            <td>Action</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <div className="flex gap-5 items-center">
+                        <div>
+                            <label htmlFor="" className="text-sm">Display</label>
+                            <select value={itemsPerPage} className="ml-2 px-2 py-1 rounded-md border-2" onChange={handlePerPage}>
                                 <option value="5">5</option>
                                 <option value="10">10</option>
                             </select>
                         </div>
                         <div>
-                            <p>Page {currentPage} of {numOfPages}</p>
+                            <p className="text-sm opacity-85">Page {currentPage} of {Math.ceil(invoices.length / itemsPerPage)}</p>
                         </div>
                     </div>
-                    <div>
-                        <div className="overflow-x-auto">
-                            <table className="table table-zebra">
-                                {/* head */}
-                                <thead>
-                                    <tr>
-                                        <th>invoice_number</th>
-                                        <th>client_code</th>
-                                        <th>Name</th>
-                                        <th>Email</th>
-                                        <th>Total</th>
-                                        <th>Paid</th>
-                                        <th> Status</th>
-                                        <th>Action</th>
 
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        currentInvoices?.map(invoice => <tr key={invoice.client_code}>
-                                            <th>1</th>
-                                            <td>Cy Ganderton</td>
-                                            <td>Quality Control Specialist</td>
-                                            <td>Blue</td>
-                                        </tr>)
-                                    }
-                                   
-                                    
-                                </tbody>
-                            </table>
-                        </div>
-
-                    </div>
                     <div className="pagination flex items-center gap-4">
                         <div onClick={handlePrevPage}>
                             <MdKeyboardArrowLeft size={20} />
                         </div>
                         {
-                            pages?.map((page) => <button key={page} className={`px-2 py-1 rounded-lg ${currentPage == page && 'selected'}`} onClick={() => setCurrentPage(page)}>{page}</button>)
+                            Array.from({ length: Math.ceil(invoices.length / itemsPerPage) }, (_, i) => (
+                                <button key={i + 1} className={`px-2 py-1 rounded-lg ${currentPage === i + 1 && 'selected'}`} onClick={() => setCurrentPage(i + 1)}>
+                                    {i + 1}
+                                </button>
+                            ))
                         }
                         <div onClick={handleNextPage}>
                             <MdKeyboardArrowRight size={20} />
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
